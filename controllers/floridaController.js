e487e05aaa8b228eb917d384f8014b60c6122e9d
Paul M Herman <>
2018-03-07 11:46:20 -0500

    adding ,gitignore

--- controllers/floridaController.js
+++ controllers/floridaController.js
@@ -5,7 +5,7 @@ var router = express.Router();
 //===================================
 
 //Import model (*.js) to use database functions
-var florida = require("<model goes here>");
+var florida = require("../models/location.js");
 //===================================
 
 //Create routes
