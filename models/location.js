f802d5040190be61e971a7b3559d90be4c8fd203
Cassie <cjrabe17@gmail.com>
2018-03-06 21:21:02 -0500

    Added model

---
+++ models/location.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+    var Locations = sequelize.define("locations", {
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        website: {
+            type: DataTypes.STRING
+        },
+        image: {
+            type: DataTypes.STRING
+        },
+        phoneNumber: {
+            type: DataTypes.STRING
+        },
+        visited: {
+            type: DataTypes.NUMBER,
+            allowNull: true
+        },
+        recommended: {
+            type: DataTypes.NUMBER,
+            allowNull: true
+        },
+        approved: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+}
+
+// Something something still need foreign key. Primary key is adding by Sequelize
