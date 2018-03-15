da88cc59b77fe342d04d8b57419f2a63767da11b
Paul M Herman <>
2018-03-14 20:08:52 -0400

    no message

--- controllers/floridaController.js
+++ controllers/floridaController.js
@@ -36,5 +36,24 @@ module.exports = {
   createUserForm: function(req, res) {
     res.render("createUserForm");
   },
+
+  approveDesitnation: function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("put condition: ", condition);
+
+    db.Location.update({
+        approved: true
+    }, condition, function(result) {
+      console.log("results from controller.js: " + result);
+        if (result.changedRows == 0) {
+          console.log("hello");
+            return res.status(404).end();
+        } else {
+            res.redirect("/adminPage");
+        }
+    });
+
+  }
 };
   
