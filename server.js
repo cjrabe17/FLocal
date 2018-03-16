43e48aa008145094aad5ad4ba278e5fd3190f1b7
Cassie <cjrabe17@gmail.com>
2018-03-15 20:45:36 -0400

    Added Some Test

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Parse application/json
 app.use(bodyParser.json());
 // Static directory
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
 // Handlebars config ---------------------------------------/
 app.engine(
