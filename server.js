119d20000862ba4d018ec7fb1c08852df01c5bb4
Cassie <cjrabe17@gmail.com>
2018-03-16 11:21:55 -0400

    added force: true to server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use("/auth", authRoutes);
 app.use("/profile", profileRoutes);
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
